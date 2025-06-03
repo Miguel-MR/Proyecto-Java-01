e29ed4d4358839833d904e50243acb6b525727c8
One PC <One PC@LAPTOP-6HLE1PVS>
2025-06-02 20:38:55 -0500

    Login básico

---
+++ src/main/java/com/mycompany/proyecto02/controller/Login.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.mycompany.proyecto02.controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ */
+@Named(value = "login")
+@SessionScoped
+public class Login implements Serializable {
+
+    /**
+     * Creates a new instance of Login
+     */
+    private String usuario;
+    private String contrasenna;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenna() {
+        return contrasenna;
+    }
+
+    public void setContrasenna(String contrasenna) {
+        this.contrasenna = contrasenna;
+    }
+    
+    public String iniciarSesion(){
+        if(usuario.equals("admin")&& contrasenna.equals("123")){
+            return "inicio";
+        }else{
+            FacesContext contexto = FacesContext.getCurrentInstance();
+            FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR,"Usuario y/o contraseña invalidos","MSG_ERROR");
+            contexto.addMessage(null, fm);
+            return null;
+        }
+    }
+    
+    public Login() {
+    }
+    
+}
+ 
